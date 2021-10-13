7b995415ed0430ca601c1803b51b0f3f3b87df24
Serikzhan Kuansyhev <kuanyshev_2020@mail.ru>
2021-10-14 03:24:09 +0600

    some tests

--- src/test/java/kz/lamps/LampsApplicationTests.java
+++ src/test/java/kz/lamps/LampsApplicationTests.java
@@ -1,13 +1,36 @@
 package kz.lamps;
 
+import kz.lamps.controller.LampController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LampsApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private LampController lampController;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        assertThat(lampController).isNotNull();
     }
 
+    @Test
+    void shouldReturnData() throws Exception {
+        this.mockMvc.perform(get("/api/lamp/getAll")).andDo(print()).andExpect(status().isOk());
+    }
 }
